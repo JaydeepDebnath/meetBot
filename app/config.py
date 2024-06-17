ee871faa25725164151d19ff86cd8bdfde5989ba
Jaydeep Debnath <132443742+JaydeepDebnath@users.noreply.github.com>
2024-06-18 01:59:36 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://sql12714397:KYxwZMjX7Q@sql12.freesqldatabase.com:3306/sql12714397'
+    SQLALCHEMY_DATABASE_URI = 'mysql://username:password@sql12.freesqldatabase.com:3306/sql12714397'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
